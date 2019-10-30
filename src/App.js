2221aaa0e3670402f87222a1ac9f3c6abf9cd641
Theo Boschetti <boschetti.theo@gmail.com>
2019-10-30 14:39:24 +0100

    commit merge

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
-<<<<<<< HEAD
 import MainPage from './components/MainPage';
 
 function App() {
   return <div className="App">
     <MainPage />
   </div>;
-=======
-import Main from "./components/Main";
-function App() {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
->>>>>>> dba25d9b6d9a0225e245b785eab62647aade5e0c
 }
 
 export default App;
