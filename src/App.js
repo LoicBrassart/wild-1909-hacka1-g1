b4a8184e81175a71ec28fff9298d3e496581c044
val-fcn <valentin.faucon@gmail.com>
2019-10-30 16:22:00 +0100

    footer done

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import MainPage from "./components/MainPage";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import FilmPage from "./components/FilmPage";
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="App">
+
       <Router>
         <Switch>
           <Route exact path="/" component={MainPage} />
           <Route path="/FilmPage" component={FilmPage} />
         </Switch>
       </Router>
+      <Footer />
     </div>
   );
 }
