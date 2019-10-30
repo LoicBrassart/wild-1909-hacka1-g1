8dc5812ec0f8f0a796a36ed0ce74103aa5ecb51f
Nicolas Merlier <nicolasmerlier@gmail.com>
2019-10-30 17:38:44 +0100

    re-re-re-re creation de la nav bar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import MainPage from "./components/MainPage";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import FilmPage from "./components/FilmPage";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-
       <Router>
         <Switch>
+          <NavBar />
           <Route exact path="/" component={MainPage} />
           <Route path="/FilmPage" component={FilmPage} />
         </Switch>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <ul className="NavbarComponents">
+        <li>
+          <img src="./fepapeur.png" />
+        </li>
+        <li>
+          <img src="./films.png" />
+        </li>
+        <li>
+          <img src="./liste.png" />
+        </li>
+        <container className="SearchBar">
+          <li className="SearchInput">
+            <input type="search" />
+          </li>
+          <li>
+            <img src="./search.png" />
+          </li>
+        </container>
+        <li>
+          <img src="mask.png" />
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
