dba25d9b6d9a0225e245b785eab62647aade5e0c
david derancourt <david.derancourt@hotmail.fr>
2019-10-30 14:33:53 +0100

    création composant main et composant filmpage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Main from "./components/Main";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
 }
 
 export default App;
