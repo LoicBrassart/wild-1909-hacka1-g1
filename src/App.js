aafbbea68df9218d4deebc3305e38033238731bf
david derancourt <david.derancourt@hotmail.fr>
2019-10-30 17:20:51 +0100

    page fonctionnelle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import MainPage from "./components/MainPage";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import FilmPage from "./components/FilmPage";
 
 function App() {
--- src/components/FilmPage.jsx
+++ src/components/FilmPage.jsx
@@ -1,11 +1,39 @@
 import React from "react";
+import Axios from "axios";
+import "./FilmPage.scss";
 
 class FilmPage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      movie: {}
+    };
   }
+
+  componentDidMount() {
+    Axios.get(
+      `https://hackathon-wild-hackoween.herokuapp.com/movies/${this.props.id}`
+    ).then(response => {
+      this.setState({ movie: response.data.movie });
+    });
+  }
+
   render() {
-    return <div></div>;
+    return (
+      <figure className="film">
+        <div className="film-div">
+          <div id="card">
+            <img src={this.state.movie.posterUrl} alt={this.state.title} />
+          </div>
+          <div className="card-info">
+            <h1>{this.state.movie.title}</h1>
+            <director>{this.state.movie.director}</director>
+            <p>description</p>
+            <button>i like this one</button>
+          </div>
+        </div>
+      </figure>
+    );
   }
 }
 
