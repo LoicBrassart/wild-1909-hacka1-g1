8d4c1f6e18b27b04804c00e6636ca8b3d1b1bfae
Philippe Lambet <philippe.lambert.fr@gmail.com>
2019-10-30 21:34:49 +0100

    remade navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,28 +4,12 @@ import "./NavBar.scss";
 function NavBar() {
   return (
     <div className="NavBar">
-      <ul className="NavbarComponents">
-        <li>
-          <img src="./fepapeur.png" />
-        </li>
-        <li>
-          <img src="./films.png" />
-        </li>
-        <li>
-          <img src="./liste.png" />
-        </li>
-        <container className="SearchBar">
-          <li className="SearchInput">
-            <input type="search" />
-          </li>
-          <li>
-            <img src="./search.png" />
-          </li>
-        </container>
-        <li>
-          <img src="mask.png" />
-        </li>
-      </ul>
+      <h1>LE SITE QUI FAIT PAS PEUR</h1>
+
+      <p>FILMS</p>
+      <p>MA LISTE</p>
+      <input type="search" value="TU VAS CREUVER!!!" />
+      <img src="mask.png" />
     </div>
   );
 }
