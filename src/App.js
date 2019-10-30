b7cc73c3d3720d3a5b116fa8c5c05eeb71b160c4
david derancourt <david.derancourt@hotmail.fr>
2019-10-30 14:44:32 +0100

    une seule mainpage et react router au bon endroit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
-import MainPage from './components/MainPage';
+import MainPage from "./components/MainPage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return <div className="App">
-    <MainPage />
-  </div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route path="/FilmPage" component={FilmPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
