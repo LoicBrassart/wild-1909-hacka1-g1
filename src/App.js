b0cba003c6f02ab08565bde1016a2d839471c761
david derancourt <david.derancourt@hotmail.fr>
2019-10-30 14:51:25 +0100

    ajout de la balise router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import MainPage from "./components/MainPage";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import FilmPage from "./components/FilmPage";
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={MainPage} />
-        <Route path="/FilmPage" component={FilmPage} />
-      </Switch>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route path="/FilmPage" component={FilmPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
